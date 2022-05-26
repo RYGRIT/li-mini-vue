171fd19858a3b0147a9888f66a273b58377523c1
Hubo <468805354@qq.com>
2022-05-26 20:29:23 +0800

    chore: init

---
+++ src/reactivity/index.ts
@@ -0,0 +1,4 @@
+// 作为 reactivity 的出口
+export function add(a, b) {
+  return a + b
+}
---
+++ src/reactivity/tests/index.spec.ts
@@ -0,0 +1,6 @@
+import { add } from '../index'
+
+it('init', () => {
+  expect(true).toBe(true)
+  expect(add(1, 1)).toBe(2)
+})
