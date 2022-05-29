5e818220541fbb5b58142505558c39df7d706124
Hubo <468805354@qq.com>
2022-05-29 21:35:14 +0800

    feat: support readonly()

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -17,3 +17,17 @@ export function reactive(raw) {
     }
   })
 }
+
+export function readonly(raw) {
+  return new Proxy(raw, {
+    get(target, key) {
+      const res = Reflect.get(target, key)
+      return res
+    },
+
+    set(target, key, newValue) {
+      // 当设置值时，应抛出异常
+      return true
+    }
+  })
+}
