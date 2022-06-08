bb897b64765bec5cb667536d48484ba22dfe13fc
Hubo <468805354@qq.com>
2022-06-08 21:35:49 +0800

    feat: 实现 isRef 功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,6 +6,7 @@ class RefImpl {
   private _value: any
   public dep
   private _rawValue: any
+  public __v_isRef = true
   constructor(value) {
     this._rawValue = value
     // value -> reactive 包裹之后的结果
@@ -43,4 +44,8 @@ function trackRefValue(ref) {
 
 export function ref(value) {
   return new RefImpl(value)
+}
+
+export function isRef(ref) {
+ return !!ref.__v_isRef 
 }
--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from "../effect"
-import { ref } from "../ref"
+import { reactive } from "../reactive"
+import { isRef, ref } from "../ref"
 
 describe('ref', () => { 
   it("happy path", () => {
@@ -39,5 +40,15 @@ describe('ref', () => {
     expect(dummy).toBe(1)
     a.value.count = 2
     expect(dummy).toBe(2)
+  })
+  
+  it('isRef', () => {
+    const a = ref(1)
+    const user = reactive({
+      age: 1
+    })
+    expect(isRef(a)).toBe(true)
+    expect(isRef(1)).toBe(false)
+    expect(isRef(user)).toBe(false)
   })
  })
