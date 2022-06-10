4b3937bd2ccbd125ec0973aaa3583f187e04b716
Hubo <468805354@qq.com>
2022-06-10 22:48:55 +0800

    feat: computed 简单实现

---
+++ src/reactivity/tests/computed.spec.ts
@@ -0,0 +1,18 @@
+import { computed } from "../computed"
+import { reactive } from "../reactive"
+
+
+describe('computed', () => { 
+  it("happy path", () => {
+    // 1. 缓存
+    const user = reactive({
+      age: 1
+    })
+
+    const age = computed(() => {
+      return user.age
+    })
+
+    expect(age.value).toBe(1)
+  })
+})
