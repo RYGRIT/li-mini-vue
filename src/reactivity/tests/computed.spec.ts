2b9045b25c01e70f02977ca99c6e4a928b6e66e6
Hubo <468805354@qq.com>
2022-06-11 21:54:26 +0800

    feat: 实现 computed 计算属性

--- src/reactivity/tests/computed.spec.ts
+++ src/reactivity/tests/computed.spec.ts
@@ -15,4 +15,36 @@ describe('computed', () => {
 
     expect(age.value).toBe(1)
   })
+
+  it("should compute lazily", () => {
+    const value = reactive({
+      foo: 1,
+    })
+    const getter = jest.fn(() => {
+      return value.foo
+    })
+    const cValue = computed(getter)
+
+    // lazy
+    expect(getter).not.toHaveBeenCalled()
+
+    expect(cValue.value).toBe(1)
+    expect(getter).toHaveBeenCalledTimes(1)
+
+    // should not computed again
+    cValue.value
+    expect(getter).toHaveBeenCalledTimes(1)
+
+    // should not computed until needed
+    value.foo = 2 // trigger -> effect -> get 重新执行了
+    expect(getter).toHaveBeenCalledTimes(1)
+
+    // now it should compute
+    expect(cValue.value).toBe(2)
+    expect(getter).toHaveBeenCalledTimes(2)
+
+    // should not compute again
+    cValue.value
+    expect(getter).toHaveBeenCalledTimes(2)
+  })
 })
