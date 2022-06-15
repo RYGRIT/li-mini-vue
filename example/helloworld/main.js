5e3b8fec147def58d47e17c5d5755cca60eede97
Hubo <468805354@qq.com>
2022-06-15 21:23:25 +0800

    feat: 初始化和实现 element 主流程

--- example/helloworld/main.js
+++ example/helloworld/main.js
@@ -3,7 +3,7 @@ import { createApp } from '../../lib/mini-vue.esm.js'
 import { App } from "./App.js"
 
 const rootContainer = document.querySelector("#app")
-createApp(App).mount("rootContainer")
+createApp(App).mount(rootContainer)
 
 
 
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,8 +1,9 @@
+import { isObject } from "../shared/index"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode, container) {
   // 调用patch方法
-  patch(vnode, container)  
+  patch(vnode, container)
 }
 
 
@@ -13,15 +14,49 @@ function patch(vnode, container) {
   // TODO: 判断vnode是不是一个element
   // 是 element 那么就应该处理element
   // 思考题：如何去区分是 element 还是 component 类型
-  // processElement()
-
-
-  processComponent(vnode, container)
+  console.log(vnode.type) // component -> object element -> div
+  if (typeof vnode.type === "string") {
+    processElement(vnode, container)
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container)
+  }
 }
 
 
 function processComponent(vnode, container) {
   mountComponent(vnode, container)
+}
+
+function processElement(vnode, container) {
+  mountElement(vnode, container)
+}
+
+function mountElement(vnode, container) {
+  // 方便理解
+  // const el = document.createElement("div")
+  // el.textContent = "hi mini-vue"
+  // el.setAttribute("id", "root")
+  // document.body.append(el)
+  const el = document.createElement(vnode.type)
+  const { children, props } = vnode
+
+  if (typeof children === "string") {
+    el.textContent = children
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, container)
+  }
+  for (const key in props) {
+    const val = props[key]
+    el.setAttribute(key, val)
+  }
+  console.log(container)
+  container.append(el)
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach(v => {
+    patch(v, container)
+  })
 }
 
 function mountComponent(vnode, container) {
