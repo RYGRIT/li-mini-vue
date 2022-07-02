1226928ed4e495cd12bc812eb205c23a01314327
Hubo <468805354@qq.com>
2022-07-02 22:19:42 +0800

    更新 element 的 children - 双端对比diff算法 - 左侧对比情况

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -3,31 +3,45 @@ import {ref, h} from "../../lib/mini-vue.esm.js"
 // 1.左侧的对比
 // (a b) c
 // (a b) d e
-const prevChildren = [
-  h("p", {key: "A"}, "A"),
-  h("p", {key: "B"}, "B"),
-  h("p", {key: "C"}, "C")
-]
+// const prevChildren = [
+//   h("p", {key: "A"}, "A"),
+//   h("p", {key: "B"}, "B"),
+//   h("p", {key: "C"}, "C")
+// ]
 
-const nextChildren = [
-  h("p", {key: "A"}, "A"),
-  h("p", {key: "B"}, "B"),
-  h("p", {key: "D"}, "D"),
-  h("p", {key: "E"}, "E")
-]
+// const nextChildren = [
+//   h("p", {key: "A"}, "A"),
+//   h("p", {key: "B"}, "B"),
+//   h("p", {key: "D"}, "D"),
+//   h("p", {key: "E"}, "E")
+// ]
 
 // 2.右侧的对比
 // a (b c)
 // d e (b c)
-// const prevChildren = [
+const prevChildren = [
+ h("p", {key: "A"}, "A"),
+ h("p", {key: "B"}, "B"),
+ h("p", {key: "C"}, "C")
+]
+
+const nextChildren = [
+ h("p", {key: "D"}, "D"),
+ h("p", {key: "E"}, "E"),
+ h("p", {key: "B"}, "B"),
+ h("p", {key: "C"}, "C")
+]
+
+// 3. 新的比老的长
+// 创建新的
+// 左侧
+// (a b)
+// (a b) c
+// i = 2, e2 = 1, e2 = 2
+// const prevChildren = [h("p", {key: "A"}, "A"), h("p", {key: "B"}, "B")]
+// const nextChildren = [
 //  h("p", {key: "A"}, "A"),
 //  h("p", {key: "B"}, "B"),
 //  h("p", {key: "C"}, "C")
 // ]
 
-// const nextChildren = [
-//  h("p", {key: "D"}, "D"),
-//  h("p", {key: "E"}, "E"),
-//  h("p", {key: "B"}, "B"),
-//  h("p", {key: "C"}, "C")
-// ]
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -123,15 +123,17 @@ export function createRenderer(options) {
     let e1 = c1.length - 1
     let e2 = c2.length - 1
 
+    function isSameVNodeType(n1, n2) {
+      // type
+      // key
+      return n1.type === n2.type && n1.key === n2.key
+    }
+
+    // 左侧
     while (i <= e1 && i <= e2) {
       const n1 = c1[i]
       const n2 = c2[i]
 
-      function isSameVNodeType(n1, n2) {
-        // type
-        // key
-        return n1.type === n2.type && n1.key === n2.key
-      }
       if (isSameVNodeType(n1, n2)) {
         patch(n1, n2, container, parentComponent)
       } else {
@@ -141,6 +143,21 @@ export function createRenderer(options) {
       i++
     }
     console.log("i", i)
+
+    // 右侧
+    while (i <= e1 && i <= e2) {
+      const n1 = c1[e1]
+      const n2 = c2[e2]
+
+      if (isSameVNodeType(n1, n2)) {
+        patch(n1, n2, container, parentComponent)
+      } else {
+        break
+      }
+
+      e1--
+      e2--
+    }
   }
   function unmountChildren(children) {
     for (let i = 0; i < children.length; i++) {
