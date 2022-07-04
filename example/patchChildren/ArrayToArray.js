625ff4144b1a76e13e4fcfb1e643484f4fd051c5
Hubo <468805354@qq.com>
2022-07-04 20:59:59 +0800

    更新 element 的 children - 双端对比diff算法 - 老的比新的长

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -48,14 +48,40 @@ import { ref, h } from "../../lib/mini-vue.esm.js"
 
 // 右侧
 // (a b)
-// c (a b)
+// d c (a b)
 // i = 0, e1 = -1, e2 = 0
 const prevChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")]
 const nextChildren = [
+  h("p", { key: "D" }, "D"),
   h("p", { key: "C" }, "C"),
   h("p", { key: "A" }, "A"),
   h("p", { key: "B" }, "B")
 ]
+
+// 4，老的比新的长
+// 删除老的
+// 左侧
+// (a b) c
+// (a b)
+// i = 2, e1 = 2, e2 = 1
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C")
+// ]
+// const nextChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")]
+
+// 右侧
+// c (a b)
+// (a b)
+// i = 0, e1 = 0, e2 = -1
+// const prevChildren = [
+//   h("p", { key: "C" }, "C"),
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B")
+// ]
+// const nextChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")]
+
 export default {
   name: "ArrayToArray",
   setup() {
